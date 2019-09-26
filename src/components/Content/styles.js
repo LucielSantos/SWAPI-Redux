b68b0020ed8a1a33bad452b41475132772568ded
LucielVitorBS <luciel.vitor.bs@gmail.com>
2019-09-25 23:12:15 -0300

    Componentes Criados

---
+++ src/components/Content/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 50vmax;
+  height:50%;
+  border: 2px solid white;
+
+  border-radius: 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap');
+
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    html, body, #root{
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    body{
+        font: 15px 'Roboto', sans-serif;
+        color: white;
+        background-color: black;
+        -webkit-font-smoothing: antialiased !important;
+    }
+`
